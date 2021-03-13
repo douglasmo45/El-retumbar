ac9d0dbd37d22135895576ce20f750891750633a
Zorrytronix <00194820@uca.edu.sv>
2021-03-13 15:08:26 -0600

    Modify: accumulators

--- accumulatorsandcounters/accumulators.cs
+++ accumulatorsandcounters/accumulators.cs
@@ -6,18 +6,18 @@ namespace accumulatorsandcounters
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("ingrese numero");
+            Console.WriteLine("please insert a number: ");
             int num = Convert.ToInt32(Console.ReadLine());
 
-            for(int i=num; i>=0; i--)
+            for(int i=1; i<=8; i++) // hace que se pueda repetir cada valor y se pueda crear el triangulo
             {
-                for(int j=0; j<i; j++)
+                for(int j=num; j>i; j--) // da el valor para poder hacer el triangulo 
                 {
                     Console.Write(j + " ");
                 }
-                for(int a=num; a<i; a++)
+                for(int a=num; a<i; a++) //bucle para los espacios para crear el triangulo
                 {
-                    Console.WriteLine();
+                    Console.WriteLine(); 
                 }
                 Console.WriteLine();
             }
